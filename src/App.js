c5d2768c143099adb87d112505af7d2fb2ac6337
Vladimir Bondar <vladi.bondar@gmail.com>
2021-10-26 00:49:54 +0300

    Profile task solution

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import './App.css';
+import Profile from './components/Profile/Profile';
+import user from './user.json';
+
+const { name, tag, location, avatar, stats } = user;
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Profile
+        name={name}
+        tag={tag}
+        location={location}
+        profileUrl={avatar}
+        stats={stats}
+      />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,49 @@
+import PropTypes from 'prop-types';
+
+function Profile({
+  name,
+  tag,
+  location,
+  profileUrl,
+  stats: { followers, views, likes },
+}) {
+  return (
+    <div className="profile">
+      <div className="description">
+        <img className="avatar" src={profileUrl} alt="Аватар пользователя" />
+        <p className="name">{name}</p>
+        <p className="tag">@{tag}</p>
+        <p className="location">{location}</p>
+      </div>
+
+      <ul className="stats">
+        <li>
+          <span className="label">Followers</span>
+          <span className="quantity">{followers}</span>
+        </li>
+        <li>
+          <span className="label">Views</span>
+          <span className="quantity">{views}</span>
+        </li>
+        <li>
+          <span className="label">Likes</span>
+          <span className="quantity">{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+Profile.propTypes = {
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  profileUrl: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number,
+    views: PropTypes.number,
+    likes: PropTypes.number,
+  }),
+};
+
+export default Profile;
