1f6fde1927780ebc124caf809f52dd88e094b813
Vladimir Bondar <vladi.bondar@gmail.com>
2021-10-29 22:58:44 +0300

    add styles

--- src/components/TransactionHistory/TransactionHistory.js
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types';
+import s from './TransactionHistory.module.css';
 
 function TransactionHistory({ transactions }) {
   return (
-    <table className='transaction-history'>
-      <thead>
+    <table className={s.transactions}>
+      <thead className={s.thead}>
         <tr>
           <th>Type</th>
           <th>Amount</th>
@@ -11,7 +12,7 @@ function TransactionHistory({ transactions }) {
         </tr>
       </thead>
 
-      <tbody>
+      <tbody className={s.tbody}>
         {transactions.map(({ id, type, amount, currency }) => (
           <tr key={id}>
             <td>{type}</td>
